09d0d01f4de1afcf54cc3e0bd659ec2610735c9c
Dave Masino <davem@slalom.com>
2023-03-23 18:31:43 -0700

    Add hackernews pipeline

--- hackernews_example.py
+++ hackernews_example.py
@@ -1,17 +1,18 @@
 import requests
+import pandas as pd
 from prefect import flow, task
-
+ 
 @task
-def get_top_story_ids(url):
+def hackernews_top_story_ids(url):
     top_story_ids = requests.get(
         url
     ).json()
     return top_story_ids[:10]
 
 @task
-def create_dataframe():
+def hackernews_top_stories(ids):
     results = []
-    for item_id in hackernews_top_story_ids:
+    for item_id in ids:
         item = requests.get(
             f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
         ).json()
@@ -21,12 +22,12 @@ def create_dataframe():
     return df
 
 @flow
-def run_pipeline():
+def hn_pipeline():
     url = "https://hacker-news.firebaseio.com/v0/topstories.json"
-    get_top_story_ids(url)
-    df = create_dataframe()
+    ids = hackernews_top_story_ids(url)
+    df = hackernews_top_stories(ids)
     
-print(run_pipeline())
+print(hn_pipeline())
 
 # @task
 # def parse_fact(response):
